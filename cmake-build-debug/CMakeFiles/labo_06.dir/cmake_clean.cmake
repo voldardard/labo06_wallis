b6e3795c867c655e643e30b6b75edfb4270181e0
Mathias Brugger <brugger.mathias@hotmail.com>
2019-10-28 16:08:00 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/labo_06.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labo_06.dir/main.cpp.o"
+  "labo_06"
+  "labo_06.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labo_06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
