21d88626202810e51159b70e506bcad97ecbc070
Mathias Brugger <brugger.mathias@hotmail.com>
2019-10-28 16:44:32 +0100

    wallis

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int numerator = 2, denominator = 1;
+
+    long double wallis_total = 1;
+    for (int iterCount = 0; iterCount < 16; ++iterCount) {
+
+        wallis_total = double(numerator) / denominator*wallis_total;
+
+        if (numerator > denominator) {
+            denominator += 2;
+        } else {
+            numerator += 2;
+        }
+
+        cout << wallis_total << endl;
+    }
+
 }
